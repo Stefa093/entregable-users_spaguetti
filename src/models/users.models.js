8320d5b3c560492ebdd14afd3bad4a68df0b1900
Stefa093 <estefaniasalazar093@gmail.com>
2022-11-28 11:31:36 -0500

    first commit

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,4 +1,46 @@
+// {
+//     id: 1,
+//     first_name: 'string',
+//     last_name: 'string',
+//     email: 'string',
+//     password: 'string',
+//     birthday: 'YYYY/MM/DD'
+// }
 
+const { DataTypes } = require('sequelize')
+
+const db = require('../utils/database')
+
+const Users = db.define('users', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        
+    },
+    password: {
+        type: DataTypes.STRING
+    },
+    birthday: {
+        type: DataTypes.STRING
+    }
+}, {
+    timestamps: false
+}) 
+
+
+module.exports = Users
 
 
 
---
+++ src/users/users.router.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+
+const userServices = require('./users.services')
+
+// - localhost:9000/api/v1/users
+// - localhost:9000/api/v1/users/:id
+
+router.get('/users', userServices.getAllUsers)
+router.post('/users', userServices.postUser)
+
+router.get('/users/:id', userServices.getUserById)
+router.patch('/users/:id', userServices.patchUser)
+router.delete('/users/:id', userServices.deleteUser)
+
+
+module.exports = router
